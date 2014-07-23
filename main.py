0a163884d7b876e98d0a0e97a4747cc79fad62e0
sergey010289 <sergeymuravko@gmail.com>
2014-07-23 22:05:15 +0300

    commit#1

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 __author__ = 'sergey'
 
+
+class dog:
+    def __init__(self, name):
+        print 'init \ndog name ->' + name
+
 if __name__ == '__main__':
     print 'this is the first project'
+    fredy = dog('Fredy')
